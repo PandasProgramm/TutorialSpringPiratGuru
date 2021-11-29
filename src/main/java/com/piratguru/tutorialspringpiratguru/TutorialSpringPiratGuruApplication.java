9155587b2783891b01b92c733f9877d49d4f363a
migue <miguel-gutierrez@hotmail.de>
2021-11-29 10:05:47 +0100

    next

--- src/main/java/com/piratguru/tutorialspringpiratguru/TutorialSpringPiratGuruApplication.java
+++ src/main/java/com/piratguru/tutorialspringpiratguru/TutorialSpringPiratGuruApplication.java
@@ -3,6 +3,8 @@ package com.piratguru.tutorialspringpiratguru;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class TutorialSpringPiratGuruApplication {
 
